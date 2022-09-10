7e1bdd0bca067df2e7f22eb57e51922be792ccb4
south-zhao <2252585312@qq.com>
2022-09-10 14:16:17 +0800

    excel

---
+++ 数据分析/excel读取.py
@@ -0,0 +1,16 @@
+# C:\Bin\envirment\python
+# -*- coding: utf-8 -*-
+# @Author  : south(南风)
+# @Time    : 2022/9/10 13:46
+# @File    : excel读取.py
+# @Software: PyCharm
+# @Describe: 读取excel表格里面的内容
+import xlrd
+
+book = xlrd.open_workbook("grade.xlsx")
+
+for sheet in book.sheets():
+    print(sheet.name)
+
+
+

8ed98fa0686f80b600d87133770c13c98c0e68a5
south-zhao <2252585312@qq.com>
2022-10-07 16:42:24 +0800

    爬虫

---
+++ 基础讲解/变量.py
@@ -0,0 +1,50 @@
+# C:\Bin\environment\python
+# -*- coding: utf-8 -*-
+# @Time : 2022/10/7 13:49
+# @Author : south(南风)
+# @File : 变量.py
+# Describe: 
+# -*- coding: utf-8 -*-
+
+"""
+变量
+    记录数据，静态和动态数据，变化的东西
+为什么要使用变量
+    为了记录事物的变化状态，方便我们编写代码
+
+怎么使用
+    先定义，再使用
+    变量名 = 值
+命名规则
+    1.只能由英文字母，数字和英文下划线组成
+    2。不能以数字开头
+    3.不能与python里面的关键字冲突
+命名变量:
+    1.开辟一个内存空间
+    2.把值放入内存空间
+    3.把内存地址与变量名绑定
+变量属性：
+    1.id  内存地址
+    2.type  数据类型
+    3.value  值
+"""
+# money = 1000
+# print(money)
+# 查看python里面的关键字
+# import keyword
+# print(keyword.kwlist)
+# money = 1000
+# k = 12.4
+# print(id(money))
+# print(id(k))
+# print(type(money))
+# print(type(k))
+a = 256
+b = 256
+print(id(a))
+print(id(b))
+
+
+
+
+

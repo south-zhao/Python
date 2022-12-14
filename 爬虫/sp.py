7ea13c377cb925ae55c90202607631e044e642eb
south-zhao <2252585312@qq.com>
2022-12-14 20:02:18 +0800

    爬虫

--- 爬虫/sp.py
+++ 爬虫/sp.py
@@ -13,7 +13,7 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
 }
 
-response = requests.get(url=url, headers=headers, verify=False)
+response = requests.get(url=url, headers=headers)
 
 print(response.text)
 
